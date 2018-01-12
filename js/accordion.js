2b0a768ece4f19f55792cc88d5bb7de89074ad6a
Dominik Woiwode <mail@dominik-woiwode.de>
2018-01-12 02:10:18 +0100

    Implement question accordion structure Add style

---
+++ js/accordion.js
@@ -0,0 +1,12 @@
+
+function addAccordionEventListener(accordion) {
+    accordion.addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
