5620089470eac397bc2192a7dda05a1fb9f91116
Dominik Woiwode <mail@dominik-woiwode.de>
2018-01-19 00:39:59 +0100

    Minor visual changes to feedback screen

--- js/feedback.js
+++ js/feedback.js
@@ -1,19 +1,17 @@
+function addFeedbackfield(button) {
+    var field = document.getElementById("feedBack_field");
+    field.style.visibility = "visible";
+    var btn = document.getElementById("feedback_btn");
+    btn.style.visibility = "visible";
 
-var counter=0;
-function addFeedbackfield(button){
-    var field=document.getElementById("feedBack_field");
-    field.style.visibility="visible";
-    var btn=document.getElementById("feedback_btn");
-    btn.style.visibility="visible";
-
-    if(button.value=="Positive"){
-        button.style.backgroundColor="Green";
-        document.getElementById("negative_btn").style.backgroundColor="#00A8CF";
+    if (button.value === "Positive") {
+        button.style.backgroundColor = "Green";
+        document.getElementById("negative_btn").style.backgroundColor = "#00A8CF";
 
     }
-    else{
-        button.style.backgroundColor="Green";
-        document.getElementById("positive_btn").style.backgroundColor="#00A8CF";
+    else {
+        button.style.backgroundColor = "Darkred";
+        document.getElementById("positive_btn").style.backgroundColor = "#00A8CF";
     }
 
 }
