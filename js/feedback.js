92f04067fe0e047b66a5644e11656860a7a9000d
Quang Phu Quang Le <quangphipx15@gmail.com>
2018-01-26 14:51:58 +0100

    Emoji for feedback added

--- js/feedback.js
+++ js/feedback.js
@@ -3,15 +3,23 @@ function addFeedbackfield(button) {
     field.style.visibility = "visible";
     var btn = document.getElementById("feedback_btn");
     btn.style.visibility = "visible";
+	
+	var pos_emj = document.getElementById("positive_emj");
+	var neg_emj = document.getElementById("negative_emj");
 
     if (button.value === "Positive") {
         button.style.backgroundColor = "Green";
         document.getElementById("negative_btn").style.backgroundColor = "#00A8CF";
-
+		
+		pos_emj.style.visibility = "visible";
+		neg_emj.style.visibility = "hidden";
     }
     else {
         button.style.backgroundColor = "Darkred";
         document.getElementById("positive_btn").style.backgroundColor = "#00A8CF";
-    }
+		
+		neg_emj.style.visibility = "visible";
+		pos_emj.style.visibility = "hidden";
+	}
 
 }
